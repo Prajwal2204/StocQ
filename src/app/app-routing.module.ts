aeef0ca110ca3582abd646d9523014a2bcc25937
Prajwal Navada <44873030+Prajwal2204@users.noreply.github.com>
2021-06-25 15:03:05 +0530

    dashboard layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'about-us', component: AboutUsComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
@@ -15,4 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, AboutUsComponent, LoginComponent]
+export const routingComponents = [HomeComponent, AboutUsComponent, LoginComponent, DashboardComponent]
