999bda38be3e45ab245fd7fdd35c1d34ce43a8dd
Prajwal Navada <44873030+Prajwal2204@users.noreply.github.com>
2021-06-29 13:33:35 +0530

    navbar progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, TemplateRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
 
+
+  dropdownMenu(input){
+    console.log(input);
+    let x = input;
+    if (x.className === "nav-items") {
+      x.className += " responsive";
+    }
+    else {
+      x.className = "nav-items";
+    }
+  }
 }
